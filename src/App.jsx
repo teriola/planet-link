b9953ce731b27e8d49764747f7f00b2861f4037b
Kristiyan <bkristiqn7@gmail.com>
2023-03-14 19:52:47 +0200

    created bookmarks page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
+import BookmarkedPosts from "./pages/Saved/BookmarkedPosts";
 import Layout from "./components/Layout";
 
 export default function App() {
@@ -19,6 +20,7 @@ hideNavigation={!isAuth}
         <Route path="/profile/about" element={<Profile />} />
         <Route path="/profile/friends" element={<Profile />} />
         <Route path="/profile/photos" element={<Profile />} />
+        <Route path="/bookmarks" element={<BookmarkedPosts />} />
 
         {/* not auth 
             <Route path="/login" element={<Login />} />
---
+++ src/pages/Saved/BookmarkedPosts.jsx
@@ -0,0 +1,11 @@
+import PostCard from "../Home/feed/PostCard";
+
+export default function BookmarkedPosts (){
+  return(
+    <>
+      <h1 className="text-6xl mb-4 text-gray-400">Bookmarks</h1>
+      <PostCard />
+      <PostCard />
+    </>
+  );
+};

85a47a6394e9e2cea3e595bcc5e035b18ec9413e
Kristiyan <bkristiqn7@gmail.com>
2023-03-22 12:55:09 +0200

    minor changes

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -20,7 +20,7 @@ export default function PostCard({ post }) {
         </div>
         <div className="grow">
           <p>
-            <Link to="/profile/posts" className="font-semibold cursor-pointer hover:underline">{firstName} {lastName}</Link>
+            <Link to={`/profile/${_id}/posts`} className="font-semibold cursor-pointer hover:underline">{firstName} {lastName}</Link>
             {' '}posted a <a href="#" className="text-blue">photo</a>
           </p>
           <p>
@@ -31,9 +31,7 @@ export default function PostCard({ post }) {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" > <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" /></svg>
         </button>
         <div className="relative">
-
           {dropdownOpen && <Dropdown />}
-
         </div>
       </div>
       <div>
